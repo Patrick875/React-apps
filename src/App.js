35d218767963dddb99a21a367f9b00ec8555543f
Patrick875 <kubwimanapatrick@gmail.com>
2020-06-28 23:15:19 +0200

    added basic functionality, next, adding switching to movie description by clicking on an  image functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//jshint esversion:9
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [movies, setMovies] = useState("");
+	const [query, setQuery] = useState("");
+	const api = {
+		url: "https://api.themoviedb.org/3/search/movie/?",
+		key: "b4f8ac6ee2c6df7cdf553494ebd88659",
+	};
+	const search = (e) => {
+		//https:api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+
+		if (e.key === "Enter") {
+			fetch(`${api.url}api_key=${api.key}&query=${query}`)
+				.then((res) => res.json())
+				.then((results) => {
+					console.log(results);
+					setMovies(results.results);
+					setQuery("");
+				});
+		} else {
+			console.log("data loading....");
+		}
+	};
+	const okay = movies
+		? movies.map((el) => {
+				//	el.poster_path = "https://image.tmdb.org/t/p/original" + el.poster_path;
+				//el.poster_path = `https://image.tmdb.org/t/p/w500/original${el.poster_path}`;
+				// let el.poster =fetch(`https://image.tmdb.org/t/p/w500/original/${el.poster_path}`).then((res)=>res);
+				return (
+					<div key={el.id} className="movie">
+						<img
+							src={`https://image.tmdb.org/t/p/w500${el.poster_path}`}
+							alt="movie img"
+						/>
+						<h4>{el.title}</h4>
+						<h5>Release Date: {el.release_date}</h5>
+						<h5>Rating: {el.vote_average}</h5>
+					</div>
+				);
+		  })
+		: "";
+	return (
+		<div className="App">
+			<main className="container">
+				<div className="input-form">
+					<input
+						type="text"
+						placeholder="search...."
+						onChange={(e) => setQuery(e.target.value)}
+						value={query}
+						onKeyPress={search}
+					/>
+					<div className="movies">{okay}</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
